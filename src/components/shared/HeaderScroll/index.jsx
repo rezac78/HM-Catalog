2252d374e075c5b7139201ee99c1f8b9d9d14b19
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2024-05-17 13:10:33 +0330

    jd

--- src/components/shared/HeaderScroll/index.jsx
+++ src/components/shared/HeaderScroll/index.jsx
@@ -1,7 +1,7 @@
 function HeaderScroll({ children, isScrolled }) {
         return (
                 <div
-                        className={`absolute md:static top-[150px] left-1/2 -translate-x-1/2 md:translate-x-[unset] flex flex-col w-[95%] md:w-full py-6 md:h-full app-content overflow-y-auto p-4 md:p-0 max-h-[80vh] md:max-h-[84vh] z-50 bg-[#FFFFFF] rounded-t-[40px] md:rounded-t-[0]`}
+                        className={`fixed md:static top-[150px] left-1/2 -translate-x-1/2 md:translate-x-[unset] flex flex-col w-[95%] md:w-full py-6 md:h-full app-content overflow-y-auto p-4 md:p-0 max-h-[80vh] md:max-h-[84vh] z-50 bg-[#FFFFFF] rounded-t-[40px] md:rounded-t-[0]`}
                 >
                         {children}
                 </div>
