079bf3d1a7e3c814dd39914c0938838991347651
reza <reza.dalvand78@gmail.com>
2024-05-19 14:57:30 +0330

    ok

--- src/components/DeskTop/CardMain/index.jsx
+++ src/components/DeskTop/CardMain/index.jsx
@@ -4,12 +4,7 @@ function CardDeskTop({ link, image, imageBack, Title }) {
   return (
     <Link to={link} className={`bg-[#1E73BF]  rounded-lg pt-6 w-[150px] h-[140px] flex flex-col items-center`}>
       <div className='relative w-[65px] h-[65px]'>
-      <img
-        alt="notfound"
-        src={imageBack}
-        className="w-full h-full z-[0] opacity-30"
-      />
-      <img alt="notfound" src={image} className="w-full h-full z-[1] -mt-[60px]" />
+      <img alt="notfound" src={image} className="w-full h-full" />
       </div>
       <span className="flex align-center justify-center text-[#ffff] py-2 text-[14px]">
         {Title}
