50d7ff7cc681e25ae9d243f123281e39ee7fed63
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2024-06-01 17:55:57 +0330

    FIX

--- src/components/shared/ListForm/index.jsx
+++ src/components/shared/ListForm/index.jsx
@@ -7,11 +7,27 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import BoaldLine from "../BoaldLine";
 
 function ListForm({ title, Data, type }) {
+  const formatText = (text) => {
+    return text.split(':').map((part, index, array) => (
+      <React.Fragment key={index}>
+        {index < array.length - 1 ? (
+          <>
+            {part}
+            :
+            <br />
+          </>
+        ) : (
+          part
+        )}
+      </React.Fragment>
+    ));
+  };
+
   return (
     <div
       className={`${
         type === "listNumber" ? "" : "mt-5 border border-blue"
-      }  rounded-t-lg`}
+      } rounded-t-lg`}
     >
       <div
         className={`${
@@ -31,7 +47,7 @@ function ListForm({ title, Data, type }) {
       <div className="flex flex-col justify-center text-right px-2 py-2 w-full">
         <span className="text-[14px] font-bold text-grayColor">
           {type === "File" ? (
-            <div className="grid grid-cols-1 md:grid-cols-2 self-center gap-3 ">
+            <div className="grid grid-cols-1 md:grid-cols-2 self-center gap-3">
               {Data.map((e) => (
                 <SoundPart key={e.sound} sound={e.sound} />
               ))}
@@ -47,27 +63,27 @@ function ListForm({ title, Data, type }) {
                   key={index}
                   className={`relative ${
                     type === "listNumber" ? "pr-0 py-1" : "pr-3 py-2"
-                  }`}
+                  } break-words`}
                 >
                   {type === "listNumber" ? (
-                    index + 1 + "_" + e.item
+                    formatText(index + 1 + "_" + e.item)
                   ) : e.type === "tags" ? (
-                    <BoaldLine matn={e.item} />
+                    <BoaldLine matn={formatText(e.item)} />
                   ) : (
                     <>
                       {type === "fagList" ? (
                         <>
-                          <span className="text-blue pl-1">{e.item}</span>
-                          <span className="">{e.item1}</span>
+                          <span className="text-blue pl-1">
+                            {formatText(e.item)}
+                          </span>
+                          <span>{formatText(e.item1)}</span>
                         </>
+                      ) : Array.isArray(e.item) ? (
+                        e.item.map((part, i) => (
+                          <div key={i}>{formatText(part)}</div>
+                        ))
                       ) : (
-                        Array.isArray(e.item) ? (
-                          e.item.map((part, i) => (
-                            <div key={i}>{part}</div>
-                          ))
-                        ) : (
-                          e.item
-                        )
+                        formatText(e.item)
                       )}
                     </>
                   )}
